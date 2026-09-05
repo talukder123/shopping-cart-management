230f7868851e8be31c5cbd4895eedaf2948f8fad
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-09-05 22:15:12 +0600

    added

--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -1,6 +1,7 @@
 import { use } from "react"
 import type { ProductType } from "../../type"
 import './Products.css'
+import Product from "../product/product"
 
 export interface ProductsProps {
     ProductPromise: Promise<ProductType[]>
@@ -13,27 +14,7 @@ export default function Products({ ProductPromise }: ProductsProps) {
     return (
         <div  className="grid-c">
             {
-                Products.map((product) => 
-                    <section className="product-style">
-                        <div className="product-card">
-                            <img src={product.image} alt={product.title} />
-
-                            <div className="product-info">
-                                <span className="product-category">{product.category}</span>
-                                <h3>{product.title}</h3>
-                                <p>{product.description}</p>
-
-                                <div className="product-rating">
-                                    ⭐ {product.rating.rate} ({product.rating.count})
-                                </div>
-
-                                <div className="product-bottom">
-                                    <span className="product-price">${product.price}</span>
-                                    <button>Add to Cart</button>
-                                </div>
-                            </div>
-                        </div>
-                    </section>)
+                Products.map((product, index) => <Product key={index} product={product}></Product> )
             }
         </div>
     )
--- src/Components/product/product.tsx
+++ src/Components/product/product.tsx
@@ -1,8 +1,34 @@
+import type { ProductType } from "../../type"
+import './product.css'
+
 export interface ProductProps {
-    prop: string
+    product: ProductType
 }
 
-export default function Product({ prop }: ProductProps) {
-    
-    return 
+export default function Product({ product }: ProductProps) {
+
+    return (
+        <div>
+            <div className="product-style">
+                <div className="product-card">
+                    <img src={product.image} alt={product.title} />
+
+                    <div className="product-info">
+                        <span className="product-category">{product.category}</span>
+                        <h3>{product.title}</h3>
+                        <p>{product.description}</p>
+
+                        <div className="product-rating">
+                            ⭐ {product.rating.rate} ({product.rating.count})
+                        </div>
+
+                        <div className="product-bottom">
+                            <span className="product-price">${product.price}</span>
+                            <button>Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
